8407ee59b4dfc544a776ba63af8a9080450c6a71
nison79 <georgenison@gmail.com>
2020-12-19 22:30:36 +0200

    THIRD

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -26,7 +26,7 @@ const AllPosts = () => {
     return (
             <div className= "min-h-screen p-12">
                 <div className="container mx-auto" >
-                    <h2 className= " pt-6 pb-24 text-base leading-6 font-bold sm:text-8xl sm:leading-7 flex justify-center">BW</h2>
+                    <h2 className= " pt-2 pb-24 text-base leading-6 font-bold sm:text-8xl sm:leading-7 flex justify-center">BW</h2>
                     <div className="flex justify-center">
                         {allPostsData &&
                         allPostsData.map((post , index) => (
